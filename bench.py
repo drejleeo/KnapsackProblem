4693336f0f63c6549bf3cbe2c397cb43335e93bf
Home <andrei.nn02@gmail.com>
2019-03-03 20:48:42 +0200

    v1.1 minor changes made, code is more readable

--- bench.py
+++ bench.py
@@ -1,28 +1,30 @@
 from utils import load_data, base_path, output_excel
 import sys
+from models import Solution
 from random import randint
 
 
 def run(nr_of_objects, default_sack, weight_limit,  given_runtimes):
-    solutions = []
 
+    solutions = []
     while given_runtimes:
         random_solution = randint(0, 2**nr_of_objects)
-        binary_solution = '{0:b}'.format(random_solution)
-        reversed_binary = binary_solution[::-1]
+        binary = '{0:b}'.format(random_solution)
+        binary_solution = (default_sack.nr_of_objects - len(binary)) * '0' + binary
         weight = 0
         quality = 0
 
-        for index in range(len(reversed_binary)):
-            if reversed_binary[index] == '1':
-                objects = default_sack.list_o
-                weight += objects[nr_of_objects-1-index].weight
-                quality += objects[nr_of_objects-1-index].value
+        for obj_index in range(default_sack.nr_of_objects):
+            if binary_solution[obj_index] == '1':
+                weight += default_sack.list_of_objects[obj_index].weight
+                quality += default_sack.list_of_objects[obj_index].value
 
         if weight <= weight_limit:
             given_runtimes -= 1
-            solutions.append((binary_solution, quality, weight))
-            print(binary_solution, quality, weight)
+            solutions.append(
+                Solution(binary_solution, weight, quality)
+            )
+            print(binary_solution, weight, quality)
 
     return solutions
 
@@ -30,10 +32,11 @@ def run(nr_of_objects, default_sack, weight_limit,  given_runtimes):
 if __name__ == '__main__':
     given_runtimes, file_rel = int(sys.argv[1]), sys.argv[2]
     total_objects, default_sack, limit = load_data(file_path='/'.join((base_path, file_rel)))
+
     solutions = run(
         nr_of_objects=total_objects,
         default_sack=default_sack,
         weight_limit=limit,
-        given_runtimes= given_runtimes,
+        given_runtimes=given_runtimes,
     )
-    output_excel(nr_of_objects=total_objects, objects=default_sack,  given_runtimes=given_runtimes, solutions=solutions)
+    output_excel(nr_of_objects=total_objects, default_sack=default_sack,  given_runtimes=given_runtimes, solutions=solutions)
--- models.py
+++ models.py
@@ -3,6 +3,7 @@ Artificial intelligence - Lab1
 Author: Leonte Andrei
 """
 
+
 class Nugat(object):
     def __init__(self, value, weight):
         self.__value = value
@@ -22,14 +23,39 @@ class Nugat(object):
 
 class Knapsack(object):
     def __init__(self):
-        self.__list_o = []
+        self.__list_of_objects = []
+        self.__nr_of_objects = 0
 
     @property
-    def list_o(self):
-        return self.__list_o
+    def list_of_objects(self):
+        return self.__list_of_objects
 
-    def put_in_knapsack(self, new_thing):
-        self.__list_o.append(new_thing)
+    @property
+    def nr_of_objects(self):
+        return self.__nr_of_objects
+
+    def put_in_sack(self, new_thing):
+        self.__list_of_objects.append(new_thing)
+        self.__nr_of_objects += 1
 
     def __str__(self):
-        return 'Knapsack!? what did u expect?'
+        return 'sack with {} objects'.format(self.nr_of_objects)
+
+
+class Solution(object):
+    def __init__(self, binary_solution, weight, quality):
+        self.__binary_solution = binary_solution
+        self.__weight = weight
+        self.__quality = quality
+
+    @property
+    def weight(self):
+        return self.__weight
+
+    @property
+    def quality(self):
+        return self.__quality
+
+    @property
+    def binary_solution(self):
+        return self.__binary_solution
