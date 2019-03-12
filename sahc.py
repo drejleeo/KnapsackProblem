a966887d85cb8355a16095fec91eec93beac0c40
Andrei LEONTE <aleonte@pentalog.com>
2019-03-13 00:18:15 +0200

    MERGE

--- sahc.py
+++ sahc.py
@@ -8,38 +8,46 @@
     5.Dupa un numar maxim de evaluari, se returneaza cel mai bun c(hilltop).
 '''
 from utils import load_data_to_instance, base_path, generate_binary_solution
-from models import Object, HypothesisBag, Knapsack
+from models import HypothesisBag
 import sys
 import time
 
 
-def sahc(current_hilltop, searching_area):
-
+def sahc(current_hilltop, searching_area, iteration=0):
     neighbours = get_neighbours(current_hilltop)
     best = find_best_neighbour(neighbours, searching_area)
-    current_quality = neighbour_intel(current_hilltop, searching_area)['quality']
 
+    current_quality = get_quality(current_hilltop, searching_area)
+    best_quality = get_quality(best, searching_area)
     # print('{} <-- current hilltop with {}'.format(current_hilltop, current_quality))
     # for nei in neighbours:
-    #     print('{} with intel: {}'.format(nei, neighbour_intel(nei, searching_area)))
+    #     print('{} with quality: {}, with weight: {}'.format(nei, get_quality(nei, searching_area), get_weight(nei, searching_area)))
     # print('\n')
     # print(best)
-    best_quality = neighbour_intel(best, searching_area)['quality']
     # print('COMPARE best: {}      WITH     current: {}'.format(best_quality, current_quality))
     # print('\n\n')
 
-    if best and best_quality > current_quality:
-        return sahc(best, searching_area)
-    else:
-        return current_hilltop
+    if best_quality > current_quality:
+        return sahc(best, searching_area, iteration + 1)
+    return current_hilltop, iteration
 
 
-def flip_bit(bin_nr, bit_index):
-    if bin_nr[bit_index] == '0':
-        bin_nr = '{}1{}'.format(bin_nr[:bit_index], bin_nr[bit_index+1:])
+def flip_bit(base2, bit_index):
+    if base2[bit_index] == '0':
+        bin_nr = '{}1{}'.format(base2[:bit_index], base2[bit_index+1:])
     else:
         return None
     return bin_nr
+
+
+def solution_to_valid(binary_solution, searching_area):
+    if is_valid(binary_solution, searching_area):
+        return binary_solution
+    for index in range(len(binary_solution)):
+        if binary_solution[index] == '1':
+            binary_solution = '{}0{}'.format(binary_solution[:index], binary_solution[index + 1:])
+            if is_valid(binary_solution, searching_area):
+                return binary_solution
 
 
 def get_neighbours(current_hilltop):
@@ -54,58 +62,72 @@ def get_neighbours(current_hilltop):
 def find_best_neighbour(neighbours, searching_area):
     quality = 0
     best = '0' * len(searching_area.list)
-    for full_intel in generate_neighbours_valid_intel(neighbours, searching_area):
-        neighbour, intel = full_intel
-        if intel['quality'] > quality:
-            quality = intel['quality']
+    for neighbour in generate_valid_neighbours(neighbours, searching_area):
+        neigh_quality = get_quality(neighbour, searching_area)
+        if neigh_quality > quality:
+            quality = neigh_quality
             best = neighbour
     return best
 
 
-def generate_neighbours_valid_intel(neighbours, searching_area):
+def generate_valid_neighbours(neighbours, searching_area):
     for neighbour in neighbours:
-        intel = neighbour_intel(binary_solution=neighbour, searching_area=searching_area)
-        if intel:
-            yield neighbour, intel
+        if is_valid(neighbour, searching_area):
+            yield neighbour
 
 
-def neighbour_intel(binary_solution, searching_area):
-    weight, quality = 0, 0
+def get_weight(binary_solution, searching_area):
+    weight = 0
     for bit_index in range(len(searching_area.list)):
         weight += searching_area.list[bit_index].weight * int(binary_solution[bit_index])
+    return weight
+
+
+def get_quality(binary_solution, searching_area):
+    quality = 0
+    for bit_index in range(len(searching_area.list)):
         quality += searching_area.list[bit_index].value * int(binary_solution[bit_index])
-        if weight > searching_area.max_weight:
-            return None
-    return {
-        'weight': weight,
-        'quality': quality,
-    }
+    return quality
+
+
+def is_valid(binary_solution, searching_area):
+    weight = get_weight(binary_solution, searching_area)
+    if weight <= searching_area.max_weight:
+        return True
+    return False
 
 
 if __name__ == '__main__':
     start_time = time.time()
-    given_runtimes, file_rel = int(sys.argv[1]), sys.argv[2]
+    iteration_limit, file_rel = int(sys.argv[1]), sys.argv[2]
     solutions = []
     all_objects, max_weight = load_data_to_instance(file_path='/'.join((base_path, file_rel)))
     hypothesis_bag = HypothesisBag(all_objects, max_weight)
+
+
+
+
+
     time2 = time.time()
-    while given_runtimes:
-        # print('\nRun {}\n'.format(given_runtimes))
+    iteration_count = 0
+    while iteration_count < iteration_limit:
         bin_sol = generate_binary_solution(len(hypothesis_bag.list))
-        if neighbour_intel(binary_solution=bin_sol, searching_area=hypothesis_bag) is None:
-            continue
-        given_runtimes -= 1
-        sol = sahc(
+        bin_sol = solution_to_valid(bin_sol, hypothesis_bag)
+        sol, iteration = sahc(
             current_hilltop=bin_sol,
             searching_area=hypothesis_bag,
         )
+        iteration_count += iteration
+
         solutions.append(sol)
+    print(get_quality(find_best_neighbour(neighbours=solutions, searching_area=hypothesis_bag), hypothesis_bag))
+
     time3 = time.time()
 
     # for sol in solutions:
         # print(neighbour_intel(sol, hypothesis_bag))
     best_sol = find_best_neighbour(neighbours=solutions, searching_area=hypothesis_bag)
-    print(best_sol, neighbour_intel(best_sol, hypothesis_bag))
+    print(best_sol, get_quality(best_sol, hypothesis_bag), get_weight(best_sol, hypothesis_bag))
     print('\n')
     print("--- %s seconds ---" % (time.time() - start_time))
     print("--- {} seconds ---".format(time3 - time2))
